2e9a1ee66f6607f31ad52d31223cb12862adda5f
devKOfori <groupfivethree@gmail.com>
2025-01-21 12:09:42 +0200

    Implement grid design for dashboard layout

--- homs/src/layouts/DashboardLayout.tsx
+++ homs/src/layouts/DashboardLayout.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
-import { Container } from '@chakra-ui/react'
+import { Container, Grid, GridItem } from '@chakra-ui/react'
 
 const DashboardLayout: React.FC<{ children: React.ReactNode}> = ({ children }) => {
+    const templateAreas = {
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`
+    }
+    const asideDisplay = { base: "none", lg: "block" }
   return (
     <div>
-      <Navbar />
-      <Sidebar />
-      <Container>
-        {children}
-      </Container>
+        <Grid templateAreas={templateAreas}>
+            <GridItem area='nav'>
+                <Navbar />
+            </GridItem>
+            <GridItem area='aside' display={asideDisplay}>
+                <Sidebar />
+            </GridItem>
+            <GridItem area='main'>
+                <Container>
+                    {children}
+                </Container>
+            </GridItem>
+        </Grid>
     </div>
   )
 }
