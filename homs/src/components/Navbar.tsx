506eeb7d68091cad616286b359b5eafffbdd85ba
devKOfori <groupfivethree@gmail.com>
2025-06-17 19:25:01 +0300

    Update Navbar design

--- homs/src/components/Navbar.tsx
+++ homs/src/components/Navbar.tsx
@@ -1,15 +1,20 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Icon, Menu, Portal } from "@chakra-ui/react";
 import { useAuth } from "../contexts/AuthProvider";
 import "./Navbar.css";
 import authService from "../services/auth-service";
 import { useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import { CiUser } from "react-icons/ci";
+import { IconButton } from "@chakra-ui/react";
+import { CiBellOn } from "react-icons/ci";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const {
     auth: { username, accessToken, refreshToken },
   } = useAuth();
-  console.log(refreshToken);
+  // console.log(refreshToken);
   const logout = () => {
     const request = authService.logout(accessToken ?? "", refreshToken ?? "");
     request.then((_) => {
@@ -36,13 +41,10 @@ const Navbar = () => {
         px="100px"
         wrap="wrap"
       >
-        <Box
-          className="logo"
-          fontWeight={600}
-        >
+        <Box className="logo" fontWeight={600}>
           HOMS
         </Box>
-        <Flex
+        {/* <Flex
           align="center"
           wrap={"wrap"}
           gap="10px"
@@ -62,7 +64,70 @@ const Navbar = () => {
           >
             Logout
           </Text>
-        </Flex>
+        </Flex> */}
+        <Box className="user-info">
+          <Flex>
+            <Box>
+              <IconButton
+                className="notification-icon"
+                _hover={{ bg: "var(--hairline-background-faint)" }}
+              >
+                <CiBellOn />
+              </IconButton>
+            </Box>
+            <Box>
+              <Menu.Root>
+                <Menu.Trigger className="user-info-trigger" asChild>
+                  <IconButton className="user-profile-icon">
+                    <Flex>
+                      <CiUser />
+                    </Flex>
+                  </IconButton>
+                </Menu.Trigger>
+                <Portal>
+                  <Menu.Positioner>
+                    <Menu.Content className="user-info-content">
+                      <Menu.Item
+                        value="new-txt"
+                        className="user-info-content-item"
+                      >
+                        My Profile
+                      </Menu.Item>
+                      <Menu.Item
+                        value="new-file"
+                        className="user-info-content-item"
+                      >
+                        Change Password
+                      </Menu.Item>
+                      <Menu.Item
+                        value="new-win"
+                        className="user-info-content-item"
+                      >
+                        Settings
+                      </Menu.Item>
+
+                      <Menu.Item
+                        value="open-file"
+                        className="user-info-content-item"
+                      >
+                        Help / Support
+                      </Menu.Item>
+                      <Menu.Item
+                        value="export"
+                        className="user-info-content-item"
+                      >
+                        Logout
+                      </Menu.Item>
+                    </Menu.Content>
+                  </Menu.Positioner>
+                </Portal>
+              </Menu.Root>
+              <Icon>
+                <RiArrowDropDownLine className="dropdown-icon" />
+              </Icon>
+            </Box>
+          </Flex>
+        </Box>
       </Flex>
     </Box>
   );
