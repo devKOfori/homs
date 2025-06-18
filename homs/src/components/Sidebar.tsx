0279cd183b8e789c4417ae7404cfa108ca9c5422
devKOfori <groupfivethree@gmail.com>
2025-06-18 12:20:36 +0300

    Update Sidebar design and toggle

--- homs/src/components/Sidebar.tsx
+++ homs/src/components/Sidebar.tsx
@@ -1,82 +1,75 @@
-import { Box, Button, Container, Flex, List, MenuItem, Stack, Text } from '@chakra-ui/react'
-import React from 'react'
-import { housekeepingMenuItems } from '../Menu'
-import { Link } from 'react-router-dom'
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  List,
+  MenuItem,
+  Stack,
+  Text,
+  IconButton,
+} from "@chakra-ui/react";
+import React from "react";
+import menuItems from "../Menu";
+import { Link } from "react-router-dom";
 import { FaAngleDoubleLeft } from "react-icons/fa";
 import { FaAngleDoubleRight } from "react-icons/fa";
-import SidebarSearch from './SidebarSearch';
-import './Sidebar.css'
-import SidebarMenuList from './SidebarMenuList';
+import { RiMenuLine } from "react-icons/ri";
+import { Tooltip } from "./ui/tooltip";
+import SidebarSearch from "./SidebarSearch";
+import "./Sidebar.css";
+import SidebarMenuList from "./SidebarMenuList";
 
-interface Props {
-  isSidebarOpen: boolean;
-  setIsSidebarOpen: (value: boolean)=>void;
-  selectedMenu: string;
-  setSelectedMenu: (value: string)=>void;
+interface SidebarProps {
+  setHideSidebar: (value: boolean) => void;
+  setSelectedMenu?: (value: string) => void;
+  hideSidebar: boolean;
 }
 
-const Sidebar = ({ isSidebarOpen, setIsSidebarOpen, selectedMenu, setSelectedMenu }: Props) => {
-
+const Sidebar = ({
+  setHideSidebar,
+  hideSidebar,
+  setSelectedMenu,
+}: SidebarProps) => {
   return (
-    // <Container bg='white' borderTopRadius='5px' height='90vh'>
-    //   <Stack>
-    //     <Box textAlign='center' p='20px' color='white' bg='#473647' borderRadius='inherit'>
-    //         <Text my='15px'>Homs</Text>
-    //         <Text my='15px'>Dashboard</Text>
-    //     </Box>
-    //     <Box borderRadius='inherit'>
-    //       <List.Root variant='plain' my='15px' px='20px'>
-    //           {housekeepingMenuItems.map((menuItem)=>(
-    //               <Link 
-    //               key={menuItem.url} 
-    //               to={`/dashboard/${menuItem.url}`}
-    //               >
-    //                   <List.Item py='5px' px='10px'
-    //                   display='block'
-    //                   _hover={{
-    //                     bg: '#DDDCDD',
-    //                     border: '1px solid #473647',
-    //                     borderRadius: '5px',
-    //                     transform: 'scale(1.05)',
-    //                     transition: 'transform 0.3s ease-out'
-    //                   }}
-    //                   >
-    //                       {menuItem.item}
-    //                   </List.Item>
-    //               </Link>
-    //           ))}
-    //       </List.Root>
-    //     </Box>
-        
-    //   </Stack>
-    // </Container>
-    <>
-      <Flex>
-        <Box>
-        <Button size='xs' 
-        h='calc(100vh - 60px)' w='23px' 
-        color='var(--link-fg)' 
-        borderRight='1px solid var(--hairline-color)'
-        onClick={()=>setIsSidebarOpen(prev=>!prev)}
-        >
-          {isSidebarOpen ? <FaAngleDoubleLeft /> : <FaAngleDoubleRight />}
-        </Button>
+    <Box className="sidebar-container">
+      {/* <Flex> */}
+      <Box
+        pt="15px"
+        width="300px"
+        className="sidebar"
+        px={hideSidebar ? "0.6rem" : "2rem"}
+      >
+        {/* <SidebarSearch /> */}
+        <Box className="hide-menu-button">
+          <IconButton
+            color="var(--logo-color)"
+            size={"xs"}
+            _hover={{
+              bg: "var(--hairline-background-faint)",
+              textDecoration: "underline",
+            }}
+            px="0.5rem"
+            onClick={() => {
+              setHideSidebar((prev) => !prev);
+            }}
+            className="hide-menu-icon"
+          >
+            {!hideSidebar ? (
+              <RiMenuLine />
+            ) : (
+              <Tooltip content="Menu" openDelay={300}>
+                <RiMenuLine />
+              </Tooltip>
+            )}{" "}
+            {!hideSidebar && "Hide Menu"}
+          </IconButton>
         </Box>
-        <Box pt='15px' width='calc(300px - 23px)'>
-          <SidebarSearch />
-          <Box bg='var(--header-bg)' 
-          letterSpacing='0.5px' 
-          textTransform='uppercase' 
-          fontWeight='400px'
-          color='var(--header-color)'
-          p='8px'
-          mt='5px'
-          >Menu</Box>
-          <SidebarMenuList data={housekeepingMenuItems} />
-        </Box>
-      </Flex>
-    </>
-  )
-}
+        {!hideSidebar && <SidebarMenuList data={menuItems} />}
+      </Box>
+      {/* </Flex> */}
+    </Box>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
