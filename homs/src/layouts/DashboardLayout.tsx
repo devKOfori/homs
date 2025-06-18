56d65b7579c31d598c063ca1ef7714fac1ec6f9f
devKOfori <groupfivethree@gmail.com>
2025-06-18 15:42:47 +0300

    Update dashboard layout

--- homs/src/layouts/DashboardLayout.tsx
+++ homs/src/layouts/DashboardLayout.tsx
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  GridItem,
+  IconButton,
+  Drawer,
+  Portal,
+} from "@chakra-ui/react";
+import { Tooltip } from "../components/ui/tooltip";
+import { RiMenuLine, RiCloseLine } from "react-icons/ri";
+import SidebarMenuList from "../components/SidebarMenuList";
+import menuItems from "../Menu";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
-  const [hideSidebar, setHideSidebar] = useState(false);
-  const [selectedMenu, setSelectedMenu] = useState("");
+  const [hideSidebar, setHideSidebar] = useState<boolean>(false);
 
   const templateAreas = {
     base: `"nav" "main"`,
@@ -35,14 +45,59 @@ const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
           overflow="hidden"
           borderRight="1px solid var(--hairline-color)"
         >
-          <Sidebar
-            setHideSidebar={setHideSidebar}
-            hideSidebar={hideSidebar}
-            setSelectedMenu={setSelectedMenu}
-          />
+          <Sidebar setHideSidebar={setHideSidebar} hideSidebar={hideSidebar} />
         </GridItem>
         <GridItem area="main" h="calc(100vh - 60px)" overflow="scroll">
-          <Box p="20px 40px">{children}</Box>
+          <Box
+            className="menu-button-on-main"
+            display={{ base: "block", lg: "none" }}
+            // pl="0.5rem"
+          >
+            <Drawer.Root placement={"start"}>
+              <Drawer.Trigger>
+                <IconButton
+                  color="var(--logo-color)"
+                  size={"xs"}
+                  _hover={{
+                    bg: "var(--hairline-background-faint)",
+                    textDecoration: "underline",
+                  }}
+                >
+                  <RiMenuLine />
+                </IconButton>
+              </Drawer.Trigger>
+              <Portal>
+                <Drawer.Backdrop />
+                <Drawer.Positioner p="3.8rem 0 0 0">
+                  <Drawer.Content bg="var(--normal-bg)" width="300px">
+                    <Drawer.Header py="0.94rem">
+                      <Drawer.Title>
+                        <IconButton
+                          color="var(--logo-color)"
+                          size={"xs"}
+                          _hover={{
+                            bg: "var(--hairline-background-faint)",
+                            textDecoration: "underline",
+                          }}
+                        >
+                          <RiMenuLine /> Menu
+                        </IconButton>
+                      </Drawer.Title>
+                    </Drawer.Header>
+                    <Drawer.Body>
+                      <SidebarMenuList data={menuItems} />
+                    </Drawer.Body>
+                    <Drawer.CloseTrigger>
+                      <IconButton>
+                        <RiCloseLine />
+                      </IconButton>
+                    </Drawer.CloseTrigger>
+                  </Drawer.Content>
+                </Drawer.Positioner>
+              </Portal>
+            </Drawer.Root>
+          </Box>
+          <Box p={{base: "1rem 0.5rem", lg:"1rem 2rem"}}>{children}</Box>
         </GridItem>
       </Grid>
     </div>
